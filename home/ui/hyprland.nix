f3c94ba7e2214fd138d11af8eb5a38de5236d186
Iury Fukuda <iuryfukuda@gmail.com>
2026-01-21 16:31:13 -0300

    fix hyprland envs

--- home/ui/hyprland.nix
+++ home/ui/hyprland.nix
@@ -16,6 +16,12 @@
       settings = {
         "$mod" = "SUPER";
 
+        env = [
+          "XDG_CURRENT_DESKTOP,Hyprland"
+          "XDG_SESSION_TYPE,wayland"
+          "XDG_SESSION_DESKTOP,Hyprland"
+        ];
+
         misc = {
           disable_hyprland_logo = true;
           force_default_wallpaper = 0;
