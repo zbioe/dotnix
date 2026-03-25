bf70a925fa3dc3b2326f394daf2d5158445ae815
Iury <iuryfukuda@gmail.com>
2026-03-25 14:55:15 -0300

    update script to generate usb key

--- iso/scripts/generate_usb_key.sh
+++ iso/scripts/generate_usb_key.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 file="$(uuid).lek"
 size="4096"
+pendrive=$1
 dd if=/dev/urandom bs=1 count=${size} >"${file}"
+dd if="${file}" of="$1" bs=1 seek=2048 count=${size} conv=fsync
 sudo cryptsetup luksAddKey /dev/disk/by-label/luks "${file}"
