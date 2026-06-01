27d0d2a51f8899072b8a6691721f332c1c6a09f5
Iury <i@zbioe.com>
2026-06-01 13:06:05 -0300

    persistence to cloudflare-warp

--- default/cloudflare.nix
+++ default/cloudflare.nix
@@ -12,6 +12,12 @@
   systemd.packages = [ unstable.cloudflare-warp ];
   systemd.user.services.warp-taskbar.wantedBy = [ "graphical.target" ];
 
+  fileSystems."/var/lib/cloudflare-warp" = {
+    device = "/var/lib/nodatacow/cloudflare-warp";
+    fsType = "none";
+    options = [ "bind" ];
+  };
+
   environment.etc."gai.conf".text = ''
     # Precedence rules to prefer IPv4 over IPv6
     precedence ::ffff:0:0/96  100
