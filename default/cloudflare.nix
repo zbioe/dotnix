ad063d2e4385e0619955b7a4eeaf5c921d5e1754
Iury <i@zbioe.com>
2026-05-31 21:29:01 -0300

    update cloudflare warp

--- default/cloudflare.nix
+++ default/cloudflare.nix
@@ -11,4 +11,9 @@
   ];
   systemd.packages = [ unstable.cloudflare-warp ];
   systemd.user.services.warp-taskbar.wantedBy = [ "graphical.target" ];
+
+  environment.etc."gai.conf".text = ''
+    # Precedence rules to prefer IPv4 over IPv6
+    precedence ::ffff:0:0/96  100
+  '';
 }
